5a8fa6e7b31e96381d62739b3f2d7c2392f6e8d0
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2025-11-04 21:46:21 +0300

    Remove database migration from Program.cs
    Removed automatic database migration on application start.

--- LeaveSystem/LeaveSystem.UI/Program.cs
+++ LeaveSystem/LeaveSystem.UI/Program.cs
@@ -50,7 +50,6 @@ namespace LeaveSystem.UI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
             app.UseSession();
             app.UseRouting();
             app.UseMiddleware<ExcpetionAndLoggingMiddleWare>();
