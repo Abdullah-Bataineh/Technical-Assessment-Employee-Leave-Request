a36183ee931e603c12ec23ec4a5c9eba1c339fd6
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2025-11-04 17:18:18 +0300

    complete created al pages with Manager and Employee

--- LeaveSystem/LeaveSystem.UI/Program.cs
+++ LeaveSystem/LeaveSystem.UI/Program.cs
@@ -50,6 +50,7 @@ namespace LeaveSystem.UI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
             app.UseSession();
             app.UseRouting();
             app.UseMiddleware<ExcpetionAndLoggingMiddleWare>();
